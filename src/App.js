48971aabdeec24207e02c00a96868d9458e0d7c5
heatblst <ashishkumar07865apk@gmail.com>
2021-02-25 05:08:09 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React ,{setState} from "react";
+import Board from "./components/Board";
+import {Button} from "react-bootstrap";
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {showModal : false}
+    this.handleClick = this.handleClick.bind(this);
+  }
+   
+  handleClick() {  
+    this.setState({
+      showModal: true,
+    });
+  }
+    render(){
+      return (
+        <div class="d-flex justify-content-center"> 
+            {!this.state.showModal ? <Button variant="primary" onClick={this.handleClick} style={{height : 60, width : 100, margin : "auto",}}>Start</Button>  : <Board />} 
+          </div>
+      )
+    }
 }
-
 export default App;
